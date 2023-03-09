34c2d887eb4e3e5cb92630644c81c62b66af8cb5
gold_one <blake120386@163.com>
2023-03-09 10:16:30 +0800

    release version 0.0.3, add ppliteseg_model

--- faceimagekit/__init__.py
+++ faceimagekit/__init__.py
@@ -1,5 +1,7 @@
 from .face_detectors import scrfd_model
 from .face_landmarks import pfld_model
+from .face_segmenters import ppliteseg_model
 from .pipelines import FaceLandmarkPipeline
 
-__all__ = ['scrfd_model', 'pfld_model', 'FaceLandmarkPipeline']
+__all__ = ['scrfd_model', 'pfld_model',
+           'ppliteseg_model', 'FaceLandmarkPipeline']
