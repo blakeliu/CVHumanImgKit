cc9067cc0cfcb07018b711ca66956713c4764a7f
gold_one <blake120386@163.com>
2023-06-13 16:35:03 +0800

    add pplitesegface12_model

--- faceimagekit/__init__.py
+++ faceimagekit/__init__.py
@@ -1,7 +1,8 @@
 from .face_detectors import scrfd_model
 from .face_landmarks import pfld_model
-from .face_segmenters import pplitesegface_model
+from .face_segmenters import pplitesegface_model, pplitesegface12_model
 from .pipelines import FaceLandmarkPipeline
 
 __all__ = ['scrfd_model', 'pfld_model',
-           'pplitesegface_model', 'FaceLandmarkPipeline']
+           'pplitesegface_model', 'FaceLandmarkPipeline',
+           'pplitesegface12_model']
