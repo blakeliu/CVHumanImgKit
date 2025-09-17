4e2e93b5aa193063f1c858769dbc7a7b75bf6588
blakeliu <blake120386@163.com>
2025-09-17 11:25:51 +0800

    fix prepare check cuda order

--- faceimagekit/engine_backends/onnx_backend.py
+++ faceimagekit/engine_backends/onnx_backend.py
@@ -8,18 +8,18 @@ from faceimagekit.core.exception import ONNXRunException
 
 if not module_available("onnxruntime"):
     raise ModuleNotFoundError(
-        "onnxtuntime package not found! please 'pip install onnxtuntime'")
+        "onnxtuntime package not found! please 'pip install onnxtuntime'"
+    )
 import onnxruntime
 
 if not module_available("GPUtil"):
-    raise ModuleNotFoundError(
-        "gputil package not found! please 'pip install gputil'")
+    raise ModuleNotFoundError("gputil package not found! please 'pip install gputil'")
 import GPUtil
 
 
 def check_onnxruntime_gpu():
     cuda = True
-    for i, r in enumerate('onnx', 'onnxruntime-gpu'):
+    for i, r in enumerate("onnx", "onnxruntime-gpu"):
         try:
             pkg.require(r)
         except Exception:
@@ -29,12 +29,9 @@ def check_onnxruntime_gpu():
 
 
 class ONNXInfer:
-    def __init__(self, weight_file,
-                 input_shape=None,
-                 output_order=None, **kwargs):
-
+    def __init__(self, weight_file, input_shape=None, output_order=None, **kwargs):
         self._model = None
-        logging.info('ONNXInfer started')
+        logging.info("ONNXInfer started")
         self.input = None
         self.input_dtype = None
         self.input_shape = input_shape  # c h w
@@ -42,32 +39,38 @@ class ONNXInfer:
         self.out_shapes = None
         self._weight_file = weight_file
         if not osp.exists(self._weight_file):
-            raise FileNotFoundError(
-                f"onnx file: {self._weight_file} not found!")
+            raise FileNotFoundError(f"onnx file: {self._weight_file} not found!")
         self.__dict__.update(**kwargs)
 
     def __del__(self):
         self._model = None
 
     # warmup
-    def prepare(self, device: str = 'cpu'):
-        cuda = (len(GPUtil.getAvailable(order='first')) >
-                0) and "cuda" in device and check_onnxruntime_gpu()
-        providers = ['CUDAExecutionProvider', 'CPUExecutionProvider'] if cuda else [
-            'CPUExecutionProvider']
+    def prepare(self, device: str = "cpu"):
+        cuda = (
+            "cuda" in device
+            and check_onnxruntime_gpu()
+            and len(GPUtil.getAvailable(order="first")) > 0
+        )
+        providers = (
+            ["CUDAExecutionProvider", "CPUExecutionProvider"]
+            if cuda
+            else ["CPUExecutionProvider"]
+        )
         self._model = onnxruntime.InferenceSession(
-            self._weight_file, providers=providers)
+            self._weight_file, providers=providers
+        )
         meta = self._model.get_modelmeta().custom_metadata_map  # metadata
         self.input = self._model.get_inputs()[0]
         self.input_dtype = self.input.type
-        if self.input_dtype == 'tensor(float)':
+        if self.input_dtype == "tensor(float)":
             self.input_dtype = np.float32
         else:
             self.input_dtype = np.uint8
         if self.output_order is None:
             self.output_order = [e.name for e in self._model.get_outputs()]
-        if 'stride' in meta:
-            stride, names = int(meta['stride']), eval(meta['names'])
+        if "stride" in meta:
+            stride, names = int(meta["stride"]), eval(meta["names"])
             self.stride = stride
             self.names = names
         logging.info("Warming up ONNX Runtime engine...")
@@ -81,14 +84,13 @@ class ONNXInfer:
 
     def run(self, input):
         try:
-            net_out = self._model.run(
-                self.output_order,
-                {self.input.name: input})
+            net_out = self._model.run(self.output_order, {self.input.name: input})
         except Exception as e:
             raise ONNXRunException(f"onnx run error: {str(e)}")
         return net_out
 
 
 def regsiter_onnx_backend(register: Registry):
-    register(fn=ONNXInfer, name=ONNXInfer.__name__,
-             namespace="engine_backend", type="onnx")
+    register(
+        fn=ONNXInfer, name=ONNXInfer.__name__, namespace="engine_backend", type="onnx"
+    )
